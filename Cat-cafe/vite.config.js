173acb819348ef0bb32f84ed9b33d82955c600b0
Rachel Eiting <reiting1@gmail.com>
2026-02-20 16:48:54 -0500

    fixed config

--- Cat-cafe/vite.config.js
+++ Cat-cafe/vite.config.js
@@ -7,8 +7,8 @@ export default defineConfig({
     react({
       babel: {
         plugins: [["babel-plugin-react-compiler"]],
-        base: "/cat_cafe_finder/",
       },
     }),
   ],
+  base: "/cat_cafe_finder/",
 });
